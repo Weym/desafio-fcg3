d279a18a51dbc9140b5df1c3267884e46fb2413e
Henry Gabriel <henrygabrielandradeoliveira@gmail.com>
2026-05-09 00:33:21 -0300

    chore(22-02): add Firebase dependencies and configure Android platform
    - Add firebase_core ^3.12.1 and firebase_messaging ^15.2.4 to pubspec.yaml
    - Configure Google Services plugin in project-level and app-level build.gradle.kts
    - Add default FCM notification channel to AndroidManifest.xml
    - Auto-generated plugin registrant files updated

--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import connectivity_plus
 import file_picker
+import firebase_core
+import firebase_messaging
 import flutter_secure_storage_macos
 import shared_preferences_foundation
 import url_launcher_macos
@@ -14,6 +16,8 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
